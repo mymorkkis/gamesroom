d0ccf911d05f51f756090f2fb9675a99b5493dc7
Max Veasey <mveasey87@gmail.com>
2018-07-05 06:41:56 +0100

    created game_enums module

---
+++ src/game_enums.py
@@ -0,0 +1,23 @@
+'''Project enums
+
+   Color: WHITE
+          BLACK
+
+   Direction: HORIZONTAL
+              DIAGONAL
+              VERTICAL
+              NON_LINEAR
+
+'''
+from enum import Enum, auto
+
+class Color(Enum):
+    WHITE = auto()
+    BLACK = auto()
+
+
+class Direction(Enum):
+    HORIZONTAL = auto()
+    DIAGONAL = auto()
+    VERTICATL = auto()
+    NON_LINEAR = auto()
