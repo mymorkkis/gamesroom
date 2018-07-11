f1b76983fc1b8cbff5afe1e46be5ba2ab3649cde
Max Veasey <mveasey87@gmail.com>
2018-07-11 08:51:13 +0100

    created Knight class and tested

---
+++ src/game_pieces/knight.py
@@ -0,0 +1,26 @@
+"""Module for Knight class."""
+from src.game_pieces.game_piece import GamePiece
+
+
+class Knight(GamePiece):
+    """Knight chess game piece. Inherits from GamePiece."""
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+
+    def valid_move(self, to_coords):
+        return self._valid(to_coords)
+
+    def valid_capture(self, to_coords):
+        return self._valid(to_coords)
+
+    def _valid(self, to_coords):
+        from_x, from_y = self.x_coord, self.y_coord
+        to_x, to_y = to_coords.x, to_coords.y
+        # Only the following 8 combinations of move/capture valid
+        if (to_x == from_x + 1 and to_y in (from_y + 2, from_y - 2)
+                or to_x == from_x + 2 and to_y in (from_y + 1, from_y - 1)
+                or to_x == from_x - 1 and to_y in (from_y + 2, from_y - 2)
+                or to_x == from_x - 2 and to_y in (from_y + 1, from_y - 1)):
+            return True
+        return False
---
+++ tests/test_knight.py
@@ -0,0 +1,35 @@
+"""Test module for Knight game piece."""
+import pytest
+
+from src.chess_game import ChessGame
+from src.game_enums import Color
+from src.game_helper import Coords
+from src.game_pieces.knight import Knight
+
+
+@pytest.fixture(scope='module')
+def knight():
+    """Setup Knight start coords. Return Knight"""
+    chess_game = ChessGame()
+    game_knight = Knight(Color.BLACK)
+    start_coords = Coords(x=4, y=4)
+    chess_game.add(game_knight, start_coords)
+    return game_knight
+
+
+@pytest.mark.parametrize('coords, rt_val', [
+    (Coords(x=5, y=6), True),
+    (Coords(x=5, y=2), True),
+    (Coords(x=6, y=5), True),
+    (Coords(x=6, y=3), True),
+    (Coords(x=3, y=6), True),
+    (Coords(x=3, y=2), True),  
+    (Coords(x=2, y=5), True),
+    (Coords(x=2, y=3), True),
+    (Coords(x=4, y=6), False),  # Can't move vertically
+    (Coords(x=2, y=4), False),  # Can't move horizontally
+    (Coords(x=5, y=5), False),  # Can't move diagonally
+    (Coords(x=2, y=8), False),  # Can't move non_linear movement more than 3 spaces
+])  # Capture also tested as uses same logic as move
+def test_knight_valid_move_and_capture(knight, coords, rt_val):
+    assert knight.valid_move(coords) == rt_val
