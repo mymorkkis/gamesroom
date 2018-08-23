0f06051636c463078844749fd9c81d723ae08d4c
Max <mveasey87@gmail.com>
2018-08-23 07:09:30 +0100

    refactored game fixture to include castle pieces

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,18 +5,24 @@ from src.chess_game import ChessGame
 from src.game_enums import Color
 from src.game_helper import add, Coords
 from src.game_pieces.king import King
+from src.game_pieces.rook import Rook
 
 
 @pytest.fixture(scope='function')
 def new_game():
+    # Start with full chess starting postions
     new_chess_game = ChessGame()
     return new_chess_game
 
 
 @pytest.fixture(scope='function')
 def game():
+    # Start with new board and pre set postions to test castling
     chess_game = ChessGame(restore_positions={})
-    # Start game with Kings for testing. Game with no Kings causes errors.
     add(King(Color.WHITE), chess_game, Coords(x=4, y=0))
     add(King(Color.BLACK), chess_game, Coords(x=4, y=7))
+    add(Rook(Color.WHITE), chess_game, Coords(x=0, y=0))
+    add(Rook(Color.WHITE), chess_game, Coords(x=7, y=0))
+    add(Rook(Color.BLACK), chess_game, Coords(x=7, y=7))
+    add(Rook(Color.BLACK), chess_game, Coords(x=7, y=7))
     return chess_game
