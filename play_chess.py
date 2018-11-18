eca39f5c21976224f8ab64a29f0a863c6e79c71a
Max <mveasey87@gmail.com>
2018-11-18 10:10:40 +0000

    refactor play_chess to use game process_input_coords method

--- play_chess.py
+++ play_chess.py
@@ -32,21 +32,11 @@ def _make_move(game):
     """
     move = input(f'{game.playing_color.value} to move. Choose wisely.....\n --> ')
     try:
-        input_from_coords, input_to_coords = move.split()
-        from_coords = _coords_from(input_from_coords)
-        to_coords = _coords_from(input_to_coords)
+        from_coords, to_coords = move.split()
+        game.process_input_coords(from_coords, to_coords)
         _clear_screen()
-        game.move(from_coords, to_coords)
     except (ValueError, KeyError):
         raise IllegalMoveError('Invalid coords, coords seperated by white space. Example usage: a1 a2')
-
-
-def _coords_from(input_coords):
-    input_x, input_y = input_coords
-    x_coords = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-    y_coords = {'1': 0, '2': 1, '3': 2, '4': 3, '5': 4, '6': 5, '7': 6, '8': 7}
-    x_coord, y_coord = x_coords[input_x], y_coords[input_y]
-    return Coords(x_coord, y_coord)
 
 
 def _clear_screen():
